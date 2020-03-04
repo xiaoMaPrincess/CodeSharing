f44a6fd9926ea917d75d401ef9fe100d66add8de
xiaomaprincess <xiaomaprincess@gmail.com>
2020-03-04 22:50:02 +0800

    选项Demo

---
+++ CodeSharing/OptionsDemo/Services/OrderServices.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsDemo.Services
+{
+    public interface IOrderServices
+    {
+        int ShowMaxOrderCount();
+    }
+    /// <summary>
+    /// 选项
+    /// </summary>
+    public class OrderServices : IOrderServices
+    {
+        //readonly IOptions<OrderServicesOptions> servicesOptions;
+        // IOptionsSnapshot 数据热更新，感知配置的数据变化，适用于范围内单例
+        readonly IOptionsSnapshot<OrderServicesOptions> servicesOptions;
+        public OrderServices(IOptionsSnapshot<OrderServicesOptions> _servicesOptions)
+        {
+            servicesOptions = _servicesOptions;
+        }
+        public int ShowMaxOrderCount()
+        {
+            return servicesOptions.Value.MaxOrderCount;
+        }
+    }
+    public class OrderServicesOptions
+    {
+        public int MaxOrderCount { get; set; } = 100;
+    }
+}
---
+++ CodeSharing/OptionsDemo/Services/OrderServicesExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsDemo.Services
+{
+    public static class OrderServicesExtensions
+    {
+        /// <summary>
+        /// 在扩展方法注册服务
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddOrderServices(this IServiceCollection services,IConfiguration configuration)
+        {
+
+            #region 配置验证
+
+            services.AddOptions<OrderServicesOptions>().Configure(x =>
+            {
+                configuration.Bind(x);
+            }).Validate(x =>
+            {
+                return x.MaxOrderCount < 100;
+            },"配置错误");// 配置验证
+            #endregion
+
+            #region 动态配置
+            services.Configure<OrderServicesOptions>(configuration);
+            // 动态配置
+            services.PostConfigure<OrderServicesOptions>(options => {
+                options.MaxOrderCount += 100;
+            });
+            #endregion
+
+            services.AddScoped<IOrderServices, OrderServices>();
+            return services;
+        }
+    }
+}
