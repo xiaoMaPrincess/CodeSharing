cb945968679e3f76293d2261a6b63d144b2dc91a
xiaomaprincess <xiaomaprincess@gmail.com>
2020-03-09 00:12:33 +0800

    Add LoggingDemo

---
+++ CodeSharing/DotNetCoreLoggingDemo/OrderServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCoreLoggingDemo
+{
+    public class OrderServices
+    {
+         ILogger<OrderServices> logger;
+        public OrderServices(ILogger<OrderServices> _logger)
+        {
+            logger = _logger;
+        }
+        public void Show()
+        {
+            logger.LogInformation("Show{time}", DateTime.Now);
+            logger.LogInformation($"Show{DateTime.Now}"); //消耗资源
+        }
+        
+    }
+}
---
+++ CodeSharing/DotNetCoreLoggingDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DotNetCoreLoggingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 配置
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.json");
+            var config = configurationBuilder.Build();
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConfiguration>(p => config);//使用工厂模式注入
+            #endregion
+            #region logging
+            // 注册服务
+            serviceCollection.AddLogging(builder =>
+            {
+                // 日志配置
+                builder.AddConfiguration(config.GetSection("Logging"));
+                builder.AddConsole();// 控制台日志
+            });
+
+            serviceCollection.AddTransient<OrderServices>();
+
+            // 构建容器
+            IServiceProvider service = serviceCollection.BuildServiceProvider();
+            #region 使用CreateLogger构建ILogger
+
+            var orderServices = service.GetService<OrderServices>();
+
+            // 获取ILoggerFactory服务
+            ILoggerFactory loggerFactory = service.GetService<ILoggerFactory>();
+            // 创建日志记录器，指定日志级别的key
+            // 使用
+            //ILogger alogger = loggerFactory.CreateLogger("Default");
+            //ILogger alogger = loggerFactory.CreateLogger<OrderServices>();
+            //alogger.LogDebug("debug");
+            //alogger.LogInformation("jee");
+            //alogger.LogError("heiheihei");
+            #endregion
+
+            #endregion
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
